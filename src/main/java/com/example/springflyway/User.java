ea4042fa336c9b63154bc776c8ca5a118a46d06e
kwongio <rldh11111@naver.com>
2025-08-11 19:43:44 +0900

    testcontainer 간단 실습

--- src/main/java/com/example/springflyway/User.java
+++ src/main/java/com/example/springflyway/User.java
@@ -1,19 +1,21 @@
 package com.example.springflyway;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
 
 }
--- src/test/java/com/example/springflyway/SpringFlywayApplicationTests.java
+++ src/test/java/com/example/springflyway/SpringFlywayApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.springflyway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringFlywayApplicationTests {
+
+    @Autowired
+    UserRepository userRepository;
+
+    // MySQL 컨테이너 생성
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.32")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    // 스프링 데이터소스 속성에 컨테이너 값 주입
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
     @Test
     void contextLoads() {
-    }
+        // 애플리케이션 컨텍스트가 정상적으로 로드되는지만 확인
+        userRepository.save(
+                User.builder()
+                        .name("Asdf")
+                        .build());
 
+    }
 }
